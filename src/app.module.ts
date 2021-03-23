0c221b0d9cb0a31461bdbff394b20fe74b3ea50c
khaalid subaan <khaalidsubaan@gmail.com>
2021-03-24 00:08:01 +0800

    setup initial graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/typeDef/schema.gql'),
+      include: [UsersModule],
+      sortSchema: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
